9dee4207b3caa111930854153421404421c0e623
YoungjunDev <96youngjun@gmail.com>
2022-04-01 17:24:19 +0900

    영준_Swagger_수정

--- src/main/java/com/dingco/pedal/config/SwaggerConfig.java
+++ src/main/java/com/dingco/pedal/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dingco.pedal.controller.*"))
+                .apis(RequestHandlerSelectors.basePackage("com.dingco.pedal.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
