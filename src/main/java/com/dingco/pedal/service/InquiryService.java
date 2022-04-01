cb98cbcd085bd0190784996624248807fa23eeb1
briso05 <sobright05@naver.com>
2022-04-01 17:24:25 +0900

    소현_Inquiry_문의정보보여주기

--- src/main/java/com/dingco/pedal/service/InquiryService.java
+++ src/main/java/com/dingco/pedal/service/InquiryService.java
@@ -1,4 +1,11 @@
 package com.dingco.pedal.service;
 
+import com.dingco.pedal.dto.InquiryDTO;
+import com.dingco.pedal.dto.MemberDTO;
+
+import java.util.List;
+
 public interface InquiryService {
+
+    public List<InquiryDTO> showUserInquiry(MemberDTO dto) throws Exception;
 }
--- src/main/java/com/dingco/pedal/service/InquiryServiceImpl.java
+++ src/main/java/com/dingco/pedal/service/InquiryServiceImpl.java
@@ -1,12 +1,21 @@
 package com.dingco.pedal.service;
 
 import com.dingco.pedal.dao.InquiryDAO;
+import com.dingco.pedal.dto.InquiryDTO;
+import com.dingco.pedal.dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("inquiryService")
 @RequiredArgsConstructor
 public class InquiryServiceImpl implements InquiryService {
 
     private final InquiryDAO dao;
+
+    @Override
+    public List<InquiryDTO> showUserInquiry(MemberDTO dto) throws Exception {
+        return dao.showUserInquiry(dto);
+    }
 }
