da334842c5c8cdf5602885b5ad0c460b5f5088e9
YoungjunDev <96youngjun@gmail.com>
2022-04-01 11:55:29 +0900

    영준_FAQDAO_selsctALl작성

--- src/main/java/com/dingco/pedal/dao/FAQDAO.java
+++ src/main/java/com/dingco/pedal/dao/FAQDAO.java
@@ -1,9 +1,21 @@
 package com.dingco.pedal.dao;
 
+import com.dingco.pedal.dto.FAQDTO;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
+@Repository("FAQDAO")
 public class FAQDAO {
 
+    @Autowired
+    SqlSession session;
+    //SqlSessionTemplate session;
 
+    public List<FAQDTO> selectAll(String category_idx)throws Exception{
+        return session.selectList("com.config.FAQMapper.FAQList",category_idx);
+    }
 }
