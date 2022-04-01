a478961961132e19609d6ce85e78bcb6403e95f5
YoungjunDev <96youngjun@gmail.com>
2022-04-01 11:57:53 +0900

    영준_Swagger_basePackager18Line추가수정(Err수정)

--- src/main/java/com/dingco/pedal/config/SwaggerConfig.java
+++ src/main/java/com/dingco/pedal/config/SwaggerConfig.java
@@ -15,9 +15,9 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dingco.pedal.*"))
+                .apis(RequestHandlerSelectors.basePackage("com.dingco.pedal.controller.*"))
                 .paths(PathSelectors.any())
                 .build();
     }
 }
-}
+
