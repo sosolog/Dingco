b9615e25ae10f3d07e3355079a0d4ad75804c35b
Woojh4930 <juhong4930@naver.com>
2022-04-12 14:37:07 +0900

    주황_로그인_로그인암호화및에러추가

--- src/main/java/com/dingco/pedal/controller/LoginController.java
+++ src/main/java/com/dingco/pedal/controller/LoginController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -44,7 +41,7 @@ public class LoginController {
 
 
     //주황 - 로그인(아이디, 비밀번호에 입력된 값을 HashMap으로 가져와서 DB와 비교)
-    @PostMapping("/login.test")
+    @PostMapping("/login")
     public String login(@Valid @ModelAttribute("loginDTO") LoginDTO loginDTO
                         , BindingResult bindingResult
                         ,@RequestParam(defaultValue = "/main") String redirectURL
@@ -70,28 +67,6 @@ public class LoginController {
     }
 
 
-    // 명지 : 로그인2 (암호화 비교)
-    @PostMapping("/login")
-    public String login2(@Valid @ModelAttribute("loginDTO") LoginDTO loginDTO
-            ,@RequestParam(defaultValue = "/main") String redirectURL
-            , BindingResult bindingResult
-            ,HttpServletRequest request) throws Exception {
-
-        if(bindingResult.hasErrors()){ return "loginForm"; }
-
-        MemberDTO loginMember = mService.selectByLoginId2(loginDTO);
-        if(loginMember==null){
-            bindingResult.reject("loginFail","아이디 또는 비밀번호가 일치하지 않습니다.");
-            return "loginForm";
-        }
-
-        //세션에 회원정보 보관
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
-
-        return "redirect:"+redirectURL;
-    }
-
     //주황 - 로그아웃
     @PostMapping("/logout")
     public String logout(HttpServletRequest request){
@@ -102,5 +77,10 @@ public class LoginController {
         return "redirect:main";
     }
 
+    //에러처리
+    @ExceptionHandler({Exception.class})
+    public String errorPage() {
+        return "error/error";
+    }
 
 }
