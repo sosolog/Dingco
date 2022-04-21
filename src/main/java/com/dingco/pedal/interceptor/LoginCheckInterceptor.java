385569c554323d4fc81c1d8a2249ade14a17dce8
Woojh4930 <juhong4930@naver.com>
2022-04-21 14:54:54 +0900

    주황_인터셉터_Session인터셉터수정

--- src/main/java/com/dingco/pedal/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/dingco/pedal/interceptor/LoginCheckInterceptor.java
@@ -1,6 +1,8 @@
 package com.dingco.pedal.interceptor;
 
+import com.dingco.pedal.annotation.Login;
 import com.dingco.pedal.dto.MemberDTO;
+import com.dingco.pedal.session.SessionConst;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,13 +19,14 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        HttpSession session = request.getSession(false);
+        HttpSession session = request.getSession();
 
+        MemberDTO memberDTO = (MemberDTO) session.getAttribute(SessionConst.LOGIN_MEMBER);
         String requestURI = request.getRequestURI();
 
-        if(session==null) {
+        if(memberDTO==null) {
             log.info("미인증 사용자 요청");
-            response.sendRedirect("/login?redirectURL="+requestURI);
+            response.sendRedirect("/login"+requestURI);
             return false;
         }
         return true;
