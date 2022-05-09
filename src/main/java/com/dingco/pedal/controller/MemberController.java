8bbcaa8c81b2b039fcc31b666426fc5ea65ff45a
YoungjunDev <96youngjun@gmail.com>
2022-05-09 17:38:48 +0900

    영준_FAQ추가작업_공지&faq분리 및  Endpoint수정

--- src/main/java/com/dingco/pedal/controller/MemberController.java
+++ src/main/java/com/dingco/pedal/controller/MemberController.java
@@ -114,7 +114,7 @@ public class MemberController {
     }
 
     // 명지 - 아이디 찾기
-    @GetMapping("/find/passwd")
+    @GetMapping("/find/id")
     public @ResponseBody String findId(@RequestParam Map<String,Object> map) throws Exception {
         String json = mService.findUserId(map);
         return json;
