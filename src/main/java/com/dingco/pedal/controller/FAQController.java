8ed6ea673033faa4a6077ee2c2565d136cd52bef
YoungjunDev <96youngjun@gmail.com>
2022-05-24 16:21:01 +0900

    영준 faq 스크롤 및 로그인 후 main 화면 변경(재수정)

--- src/main/java/com/dingco/pedal/controller/FAQController.java
+++ src/main/java/com/dingco/pedal/controller/FAQController.java
@@ -45,7 +45,7 @@ public class FAQController {
         int curPage = Integer.parseInt(cp);
 
         // 검색 조건에 맞는 데이터 조회(+페이징)
-        PageDTO<FAQDTO> pageDTO = faqService.selectNOTICESearchRecord(curPage, searchKey);
+        PageDTO<FAQDTO> pageDTO = faqService.selectFAQSearchRecord(curPage, searchKey);
 
         return pageDTO;
     }
@@ -110,73 +110,4 @@ public class FAQController {
         return result;
     }
 
-////////// 기본 페이징 처리(관리자 페이지에서 이용) //////////
-
-    /**
-     * FAQ 전체 조회(페이징)
-     * @param curPage : 현재 페이지(쿼리 스트링으로 들고 옴)
-     * @param request : page.jsp에서 <a> 태그를 사용하여 이동할 때 참고하는 현재 위치의 절대 주소값(request.getServletPath())
-     * @return FaqList로 이동
-     */
-    @GetMapping("/faq_admin")
-    public String faq(@RequestParam(value = "pg", required = false, defaultValue = "1") String curPage,
-                      HttpServletRequest request,
-                      Model model) throws Exception {
-
-        // 전체 데이터 조회(+페이징)
-        PageDTO<FAQDTO> pageDTO = faqService.selectFAQRecordPaging(Integer.parseInt(curPage));
-
-        // 조건(1) : 검색되는 값이 존재하지 않으면 전체 페이지가 0이 되고 현재 페이지가 1(default)이라서 조건(2)의 else 조건문이 실행
-        // 검색되는 값이 존재하지 않는 값이 있더라도 조회가 될 수 있도록 먼저 조건문 배치
-        if(Integer.parseInt(curPage) == 1 && pageDTO.getTotalPage() == 0){
-            model.addAttribute("pageDTO", pageDTO);
-
-            return "FaqList";
-        }
-        // 조건(2) : 전체 페이지 범위를 초과해서 조회할 경우에는 redirect:/faq를 통해서 FAQ로 돌아가게끔 세팅
-        if(Integer.parseInt(curPage) > pageDTO.getTotalPage()) {
-            return "redirect:/faq";
-        }
-        else {
-            model.addAttribute("pageDTO", pageDTO);
-
-            return "FaqList";
-        }
-    }
-
-    /**
-     * 검색 조건에 맞는 FAQ 부분 조회(페이징)
-     * @param curPage : 현재 페이지(쿼리 스트링으로 들고 옴)
-     * @param searchKey : 찾을 문자열(검색 조건)
-     * @param request : searchPage.jsp에서 <a> 태그를 사용하여 이동할 때 참고하는 현재 위치의 절대 주소값(request.getServletPath())
-     * @return FaqSearchList로 이동(path)
-     */
-    @GetMapping("/faq_admin/search")
-    public String faqSearch(@RequestParam(value = "pg", required = false, defaultValue = "1") String curPage,
-                            @RequestParam(value = "sch", required = false) String searchKey,
-                            HttpServletRequest request,
-                            Model model) throws Exception {
-
-        // 검색 조건에 맞는 데이터 조회(+페이징)
-        PageDTO<FAQDTO> pageDTO = faqService.selectFAQSearchRecordPaging(Integer.parseInt(curPage), searchKey);
-
-        // 조건(1) : 검색되는 값이 존재하지 않으면 전체 페이지가 0이 되고 현재 페이지가 1(default)이라서 조건(2)의 else 조건문이 실행
-        // 검색되는 값이 존재하지 않는 값이 있더라도 조회가 될 수 있도록 먼저 조건문 배치
-        if(Integer.parseInt(curPage) == 1 && pageDTO.getTotalPage() == 0){
-            model.addAttribute("pageDTO", pageDTO);
-            model.addAttribute("searchKey", searchKey);
-
-            return "FaqSearchList";
-        }
-        // 조건(2) : 전체 페이지 범위를 초과해서 조회할 경우에는 redirect:/faq를 통해서 FAQ로 돌아가게끔 세팅
-        if(Integer.parseInt(curPage) > pageDTO.getTotalPage()) {
-            return "redirect:/faq";
-        }
-        else {
-            model.addAttribute("pageDTO", pageDTO);
-            model.addAttribute("searchKey", searchKey);
-
-            return "FaqSearchList";
-        }
-    }
 }
