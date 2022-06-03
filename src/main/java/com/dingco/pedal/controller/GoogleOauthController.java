9bbe05b84a338cc8d1cd5f107e7f28e5ac9b36ab
Woojh4930 <juhong4930@naver.com>
2022-06-03 15:20:08 +0900

    GoogleOauthController_주석_주황

--- src/main/java/com/dingco/pedal/controller/GoogleOauthController.java
+++ src/main/java/com/dingco/pedal/controller/GoogleOauthController.java
@@ -31,7 +31,40 @@ public class GoogleOauthController {
     @Value("${sns.google.callback.url}")
     private String GOOGLE_SNS_CALLBACK_URL;
 
-    //    주황 - 구글 로그인 인증코드
+    /**
+    * 주황 - 구글 로그인 로그인폼
+     * 회원정보가 없으면 나타나는 회원가입화면
+    * */
+    @GetMapping(value = "/login/oauth/google.form")
+    public String googleLoginForm() throws Exception {
+
+        return "googleLoginForm";
+
+    }
+
+    /**
+     * 주황 - 구글 로그인 로그인확인
+     * memberDTO - googleLoginForm에서 입력한 이름과 아이디와 googleIdx
+     * 입력한 정보 DB에 저장 후 로그인
+     * */
+    @PostMapping(value = "/login/oauth/google.action")
+    public String loginCheck(MemberDTO memberDTO,HttpServletRequest request) throws Exception {
+        mService.memberGoogleAdd(memberDTO);
+        HttpSession session = request.getSession();
+
+        session.setAttribute(SessionConst.LOGIN_MEMBER,memberDTO);
+        return "redirect:/main";
+
+    }
+
+    /**
+     * 주황 - 구글 로그인 인증코드
+     * googleUrl - access_token 도출에 필요한 code를 얻기 위해 접근하는 url
+     * scope - 접근할 수 있는 권한
+     * client_id - Google Cloud Platform에서 요청한 클라이언트 아이디
+     * redirect_uri - 정보를 도출하기 위해 access_token을 전달하는 곳
+     * response_type, access_type, state는 동일한 값을 입력한다
+     * */
     @GetMapping("/login/oauth/google")
     @ResponseBody
     public void loginPage(HttpServletResponse response) throws IOException {
@@ -47,7 +80,12 @@ public class GoogleOauthController {
 
     }
 
-    //    주황 - 구글 로그인 콜백
+    /**
+     * 주황 - 구글 로그인 콜백
+     * redirectAttributes - 정보 전달할 때 쿼리스트링으로 넘어가는 방식을 url에서 안보이게 하는 것
+     * @RequestParam("code") - access_token을 얻을 수 있는 코드
+     *
+     * */
     @GetMapping(value = "/login/oauth/google/callback")
     public String googleLogin(@RequestParam("code") String code,
                               RedirectAttributes redirectAttributes,
@@ -60,6 +98,7 @@ public class GoogleOauthController {
 
         // Access Token으로 사용자 정보 반환
         JsonNode userInfo = GoogleLogin.getGoogleUserInfo(accessToken);
+
 
         String google_idx = userInfo.get("names").get(0).get("metadata").get("source").get("id").asText();
 
@@ -80,26 +119,6 @@ public class GoogleOauthController {
 
 
         return next;
-    }
-
-    //    주황 - 구글 로그인 로그인폼
-    @GetMapping(value = "/login/oauth/google.form")
-    public String googleLoginForm() throws Exception {
-
-        return "googleLoginForm";
-
-    }
-
-//    주황 - 구글 로그인 로그인확인
-
-    @PostMapping(value = "/login/oauth/google.action")
-    public String loginCheck(MemberDTO memberDTO,HttpServletRequest request) throws Exception {
-        mService.memberGoogleAdd(memberDTO);
-        HttpSession session = request.getSession();
-
-        session.setAttribute(SessionConst.LOGIN_MEMBER,memberDTO);
-        return "redirect:/main";
-
     }
 
 
